2811b517e07b4a4483f2bbc255de440615cdce73
Tiago <tiago.riscado@ubi.pt>
2024-03-23 19:51:22 +0000

    Teste

--- base.py
+++ base.py
@@ -2,8 +2,18 @@ import pandas as pd
 
 #Criação da data frame
 
-path = r"data\all_perth_310121.csv"
+path1 = r"data\all_perth_310121.csv"
+path2 = r"data\Melbourne_housing_FULL.csv"
+#path3 = r"data\melbourne_housing"
 
-df = pd.read_csv(path)
+df1 = pd.read_csv(path1)
+df2 = pd.read_csv(path2)
+#df3 = pd.read_csv(path3)
 
-#print(df)
+#print(df1)
+#print(df2)
+#print(df3)
+
+
+
+
