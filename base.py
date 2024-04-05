f5377345efac4df5c53789bf14fa22b6d341e2fc
Tiago <tiago.riscado@ubi.pt>
2024-04-05 11:13:21 +0100

    Teste

--- base.py
+++ base.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 
-#Criação das dataframes
-
+# Criação das dataframes
 path1 = r"data\all_perth_310121.csv"
 path2 = r"data\Melbourne_housing_FULL.csv"
-path3 = r"data\melbourne_housing.csv"
+path3 = r"data\NY-House-Dataset.csv"
 
 df1 = pd.read_csv(path1)
 df2 = pd.read_csv(path2)
@@ -13,39 +12,30 @@ df3 = pd.read_csv(path3)
 
 # Renomeação das colunas
 df1 = df1.rename(columns={'LAND_AREA': 'Landsize'})
-df2 = df2.rename(columns={'Bedroom2': 'BEDROOMS', 'Bathrooms': 'BATHROOMS', 'Car': 'GARAGE', 'YearBuilt': 'BUILD_YEAR', 'Address': 'ADDRESS', 'Price': 'PRICE', 'Suburb': 'SUBURB', 'Postcode': 'POSTCODE', 'Lattitude': 'LATITUDE', 'Longtitude': 'LONGITUDE'})
-df3 = df2.rename(columns={'Bedroom2': 'BEDROOMS', 'Bathrooms': 'BATHROOMS', 'Car': 'GARAGE', 'YearBuilt': 'BUILD_YEAR', 'Address': 'ADDRESS', 'Price': 'PRICE', 'Suburb': 'SUBURB', 'Postcode': 'POSTCODE', 'Lattitude': 'LATITUDE', 'Longtitude': 'LONGITUDE'})
+df2 = df2.rename(columns={'Bedroom2': 'BEDROOMS', 'Bathroom': 'BATHROOMS', 'Car': 'GARAGE', 'YearBuilt': 'BUILD_YEAR', 'Address': 'ADDRESS', 'Price': 'PRICE', 'Suburb': 'SUBURB', 'Postcode': 'POSTCODE', 'Lattitude': 'LATITUDE', 'Longtitude': 'LONGITUDE', 'Date': 'DATE_SOLD'})
+df3 = df3.rename(columns={'BEDS': 'BEDROOMS', 'BATH': 'BATHROOMS', 'PROPERTYSQFT': 'Landsize', })
 
 # Remoção de colunas
 df1 = df1.drop(columns={'FLOOR_AREA', 'CBD_DIST', 'NEAREST_STN', 'NEAREST_STN_DIST', 'NEAREST_SCH', 'NEAREST_SCH_DIST', 'NEAREST_SCH_RANK'})
 df2 = df2.drop(columns={'Rooms', 'Type', 'Method', 'SellerG', 'BuildingArea', 'CouncilArea', 'Regionname', 'Propertycount', 'Distance'})
-df3 = df3.drop(columns= {'Rooms', 'Type', 'Method', 'SellerG', 'BuildingArea', 'CouncilArea', 'Regionname', 'Propertycount', 'Distance'})
+df3 = df3.drop(columns={'BROKERTITLE', 'TYPE', 'STATE', 'MAIN_ADDRESS', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LOCALITY', 'SUBLOCALITY', 'STREET_NAME', 'LONG_NAME', 'FORMATTED_ADDRESS' } )
 
 # Concatenação das dataframes
 df = pd.concat([df1, df2, df3], ignore_index=True)
 
-# Substituir valores não finitos (NaN, infinito) por NaN
-df['PRICE'] = df['PRICE'].replace([np.inf, -np.inf], np.nan)
-df['POSTCODE'] = df['POSTCODE'].replace([np.inf, -np.inf], np.nan)
-df['BEDROOMS'] = df['BEDROOMS'].replace([np.inf, -np.inf], np.nan)
-df['BATHROOMS'] = df['BATHROOMS'].replace([np.inf, -np.inf], np.nan)
-df['GARAGE'] = df['GARAGE'].replace([np.inf, -np.inf], np.nan)
-df['BUILD_YEAR'] = df['BUILD_YEAR'].replace([np.inf, -np.inf], np.nan)
-
 # Conversão de float para inteiro
 
-df['PRICE'] = pd.to_numeric(df['PRICE'], errors='coerce').astype(pd.Int64Dtype())
-df['POSTCODE'] = pd.to_numeric(df['POSTCODE'], errors='coerce').astype(pd.Int64Dtype())
-df['BEDROOMS'] = pd.to_numeric(df['BEDROOMS'], errors='coerce').astype(pd.Int64Dtype())
-df['BATHROOMS'] = pd.to_numeric(df['BATHROOMS'], errors='coerce').astype(pd.Int64Dtype())
-df['GARAGE'] = pd.to_numeric(df['GARAGE'], errors='coerce').astype(pd.Int64Dtype())
-df['BUILD_YEAR'] = pd.to_numeric(df['BUILD_YEAR'], errors='coerce').astype(pd.Int64Dtype())
+numeric_columns = ['PRICE', 'POSTCODE', 'BEDROOMS', 'BATHROOMS', 'GARAGE', 'BUILD_YEAR']
+for col in numeric_columns:
+    df[col] = pd.to_numeric(df[col], errors='coerce').round(decimals=0).astype('Int64')
 
-# Remoção de linhas com valores nulos
-df = df.drop(columns={'Date', 'Bathroom'})
+df['DATE_SOLD'] = pd.to_datetime(df['DATE_SOLD'], errors='coerce')
 
-#Criação do dataset file
+# Formatar a coluna de data como "dia/mês/ano"
+df['DATE_SOLD'] = df['DATE_SOLD'].dt.strftime('%d/%m/%Y')
+# Remover colunas vazias
 
+# Criação do dataset file
 df.to_csv('data/dataset.csv', index=False)
 
 # Carregar o dataset
--- missing_values.py
+++ missing_values.py
@@ -1,7 +1,15 @@
 from base import *
-
+import matplotlib.pyplot as plt
 # Contagem de valores em falta
 
 missing_values = df.isnull().sum()
-print(missing_values)
+#print(missing_values)
+
+missing_values1 = df1.isnull().sum()
+missing_values2 = df2.isnull().sum()
+missing_values3 = df3.isnull().sum()
+# print(missing_values1)
+# print(missing_values2)
+# print(missing_values3)
+
 
