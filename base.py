1794ee37a2bd8da3742546cb3a4b8f269fc063e2
Tiago <tiago.riscado@ubi.pt>
2024-04-07 18:22:44 +0100

    Base

--- base.py
+++ base.py
@@ -4,24 +4,24 @@ import numpy as np
 # Criação das dataframes
 path1 = r"data\all_perth_310121.csv"
 path2 = r"data\Melbourne_housing_FULL.csv"
-path3 = r"data\NY-House-Dataset.csv"
+#path3 = r"data\NY-House-Dataset.csv"
 
 df1 = pd.read_csv(path1)
 df2 = pd.read_csv(path2)
-df3 = pd.read_csv(path3)
+#df3 = pd.read_csv(path3)
 
 # Renomeação das colunas
 df1 = df1.rename(columns={'LAND_AREA': 'Landsize'})
 df2 = df2.rename(columns={'Bedroom2': 'BEDROOMS', 'Bathroom': 'BATHROOMS', 'Car': 'GARAGE', 'YearBuilt': 'BUILD_YEAR', 'Address': 'ADDRESS', 'Price': 'PRICE', 'Suburb': 'SUBURB', 'Postcode': 'POSTCODE', 'Lattitude': 'LATITUDE', 'Longtitude': 'LONGITUDE', 'Date': 'DATE_SOLD'})
-df3 = df3.rename(columns={'BEDS': 'BEDROOMS', 'BATH': 'BATHROOMS', 'PROPERTYSQFT': 'Landsize', })
+#df3 = df3.rename(columns={'BEDS': 'BEDROOMS', 'BATH': 'BATHROOMS', 'PROPERTYSQFT': 'Landsize', })
 
 # Remoção de colunas
 df1 = df1.drop(columns={'FLOOR_AREA', 'CBD_DIST', 'NEAREST_STN', 'NEAREST_STN_DIST', 'NEAREST_SCH', 'NEAREST_SCH_DIST', 'NEAREST_SCH_RANK'})
 df2 = df2.drop(columns={'Rooms', 'Type', 'Method', 'SellerG', 'BuildingArea', 'CouncilArea', 'Regionname', 'Propertycount', 'Distance'})
-df3 = df3.drop(columns={'BROKERTITLE', 'TYPE', 'STATE', 'MAIN_ADDRESS', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LOCALITY', 'SUBLOCALITY', 'STREET_NAME', 'LONG_NAME', 'FORMATTED_ADDRESS' } )
+#df3 = df3.drop(columns={'BROKERTITLE', 'TYPE', 'STATE', 'MAIN_ADDRESS', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LOCALITY', 'SUBLOCALITY', 'STREET_NAME', 'LONG_NAME', 'FORMATTED_ADDRESS' } )
 
 # Concatenação das dataframes
-df = pd.concat([df1, df2, df3], ignore_index=True)
+df = pd.concat([df1, df2], ignore_index=True)
 
 # Conversão de float para inteiro
 
--- missing_values.py
+++ missing_values.py
@@ -3,13 +3,34 @@ import matplotlib.pyplot as plt
 # Contagem de valores em falta
 
 missing_values = df.isnull().sum()
-#print(missing_values)
+print(missing_values)
 
 missing_values1 = df1.isnull().sum()
 missing_values2 = df2.isnull().sum()
-missing_values3 = df3.isnull().sum()
-# print(missing_values1)
-# print(missing_values2)
-# print(missing_values3)
+#missing_values3 = df3.isnull().sum()
+print(missing_values1)
+print(missing_values2)
+#print(missing_values3)
+
+# Função para plotar histograma de valores ausentes
+def plot_missing_histogram(dataframe, title):
+    missing_values_count = dataframe.isnull().sum()
+    plt.figure(figsize=(10, 6))
+    plt.bar(missing_values_count.index, missing_values_count.values, color='skyblue', edgecolor='black')
+    plt.title(title)
+    plt.xlabel('Colunas')
+    plt.ylabel('Contagem de Valores Ausentes')
+    plt.xticks(rotation=45)
+    plt.show()
+
+# Plotar histograma de valores ausentes para o primeiro conjunto de dados (Perth)
+plot_missing_histogram(df1, 'Histograma de Valores Ausentes - Perth')
+
+# Plotar histograma de valores ausentes para o segundo conjunto de dados (Melbourne)
+plot_missing_histogram(df2, 'Histograma de Valores Ausentes - Melbourne')
+
+# Plotar histograma de valores ausentes para o dataset final
+plot_missing_histogram(df, 'Histograma de Valores Ausentes - Dataset')
+
 
 
