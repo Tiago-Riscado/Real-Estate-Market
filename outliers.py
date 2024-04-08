30de2ff5a8e0fe23750983688c90712570f36e89
carolinarraposo <carolina.reis.raposo@ubi.pt>
2024-04-08 18:36:48 +0100

    outliers

--- outliers.py
+++ outliers.py
@@ -1,31 +1,40 @@
+#importação
 from base import *
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import time
+#selecionar colunas do dataset que contêm dados numéricos (inteiros ou float)
 columns = df.select_dtypes(include=['int', 'float']).columns
 
+#método IQR
+#calcular quartis
 Q1 = df[columns].quantile(0.25)
 Q3 = df[columns].quantile(0.75)
 
+#calcular amplitude interquartil
 IQR = Q3 -Q1
 
-limite_inf = Q1 - 1.5 * IQR
-limite_sup = Q3 + 1.5 * IQR
+#calcular os limites inferior e superior
+lim_inf = Q1 - 1.5 * IQR
+lim_sup = Q3 + 1.5 * IQR
 
-outliers = (df[columns] < limite_inf) | (df[columns] > limite_sup)
+#determinação dos outliers (se o valor for inferior ao limite inferior ou superior ao limite superior)
+outliers = (df[columns] < lim_inf) | (df[columns] > lim_sup)
 
+#número de outliers por coluna
 num_outliers_col = outliers.sum()
 
 print("Quantidade de Outliers:")
 print(num_outliers_col)
 
-# Plotar gráficos de dispersão para cada coluna
-for col in columns:
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(x=df.index, y=df[col], hue=outliers[col], palette='Set2')
-    plt.title(f'Dispersão de {col}')
-    plt.xlabel('Índice')
-    plt.ylabel(col)
-    plt.legend(title='Outlier', loc='upper right')
-    plt.show()
+#Gráficos de dispersão
+for i, col1 in enumerate(columns):
+    for j, col2 in enumerate(columns):
+        if i != j:
+            plt.figure(figsize=(9,9))
+            sns.scatterplot(x=col1, y=col2, data=df)
+            plt.xlabel(col1)
+            plt.ylabel(col2)
+            plt.title(f'Scatter plot: {col1} vs {col2}')
+            plt.show()
 
